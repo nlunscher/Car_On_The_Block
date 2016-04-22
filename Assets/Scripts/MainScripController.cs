78d0bd74c66dbacba026a69f96eb86fa0d72c7b7
nlunscher <nolan_lunscher@hotmail.com>
2016-04-22 16:56:17 -0400

    Added screenshots

--- Assets/Scripts/MainScripController.cs
+++ Assets/Scripts/MainScripController.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System;
 
 public class MainScripController : MonoBehaviour {
 
@@ -8,12 +9,20 @@ public class MainScripController : MonoBehaviour {
 	public Camera carCamera;
 	public Text viewText;
 
+	public bool constantShots = false;
+
+	private bool takeHiRexShot = false;
+	private int resWidth = 480;
+	private int resHeight = 360;
+	private TimeSpan screenInterval = TimeSpan.FromMilliseconds(500);
+	private DateTime lastScreen;
 
 	// Use this for initialization
 	void Start () {
 		overHeadCamera.enabled = true;
 		carCamera.enabled = false;
 		viewText.text = "Overhead View";
+		lastScreen = DateTime.Now - screenInterval;
 	}
 	
 	// Update is called once per frame
@@ -23,6 +32,17 @@ public class MainScripController : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
 			switchCameras();
+		}
+
+		// take screenshot with 'k'
+		if (Input.GetKeyDown("k"))
+		{
+			takeScreenShot();
+		}
+		// take screenshot every screenInterval
+		if (constantShots && DateTime.Now - lastScreen >= screenInterval) {
+			takeScreenShot();
+			lastScreen = DateTime.Now;
 		}
 	}
 
@@ -40,5 +60,31 @@ public class MainScripController : MonoBehaviour {
 		{
 			viewText.text = "Car View";
 		}
+	}
+
+	public static string ScreenShotName(int width, int height)
+	{
+		string name = string.Format ("{0}/ScreenShots/screen_{1}x{2}_{3}.jpg",
+			              Application.dataPath,
+			              width, height,
+			              System.DateTime.Now.ToString ("yyyy-MM-dd_HH_mm-ss.ffff"));
+		return name;
+	}
+
+	void takeScreenShot()
+	{
+		RenderTexture rt = new RenderTexture (resWidth, resHeight, 24);
+		carCamera.targetTexture = rt;
+		Texture2D screenShot = new Texture2D (resWidth, resHeight, TextureFormat.RGB24, false);
+		carCamera.Render ();
+		RenderTexture.active = rt;
+		screenShot.ReadPixels (new Rect (0, 0, resWidth, resHeight), 0, 0);
+		carCamera.targetTexture = null;
+		RenderTexture.active = null; // added to avoid errors
+		Destroy(rt);
+		byte[] bytes = screenShot.EncodeToJPG ();
+		string fileName = ScreenShotName (resWidth, resHeight);
+		System.IO.File.WriteAllBytes (fileName, bytes);
+		Debug.Log (string.Format ("Took screenshot to: {0}", fileName));
 	}
 }
